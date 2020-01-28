21571812ea6401f4f5477af96400e5b0ccbf155f
WilsonCedillo <wcedillo@fwparker.org>
2020-01-28 14:26:14 -0600

    Day 1

---
+++ Notes/01_Modules.py
@@ -0,0 +1,19 @@
+#Modules, Imports, and Libaries
+
+import turtle
+from math import pi, cos  # import single items from a lib
+from random import *  # imports everything locally
+import random as r  # use you own keyboard
+import Import_me
+
+if __name__== "__main__":
+    # This code only runs when you run this file first
+    print("Hello")
+    # my_turtle = turtle.Turtle()
+    print(pi)
+    print(cos(0.5))
+    print(randrange(1,7))
+    print(r.random)
+    print(Import_me.y)
+
+
