40484a1f397fe2a28c2736daa2d7d911416e0b25
WilsonCedillo <wcedillo@fwparker.org>
2020-02-06 13:02:08 -0600

    O2_lists Homework

---
+++ Problems/Hangman.py
@@ -0,0 +1,34 @@
+for i in range(5, 51, 5):
+    print(i, end=" ")
+
+my_list = [x for x in range(100)]
+print(my_list)
+
+
+my_list = list(range(100))
+print(my_list)
+
+
+for number in my_list:
+    if number > 10:
+        break
+    print(number, end="Wilson")
+
+print("End of loop")
+
+# Continue
+
+for number in my_list:
+    if number % 7 == 0:
+        continue
+    print(number, end=" ")
+
+for number in my_list:
+    print(number, end=" ")
+    if number == 80:
+        break
+else:
+    print("The loop completed naturally")
+
+
+
