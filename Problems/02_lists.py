2545f6b2b5377c029f4bdedb0662391485f67a8e
WilsonCedillo <wcedillo@fwparker.org>
2020-02-11 14:23:49 -0600

    Searching.py

--- Problems/02_lists.py
+++ Problems/02_lists.py
@@ -45,11 +45,42 @@ my_counts = [num_list.count(x) for x in num_list]
 print(my_counts)
 print(num_list[my_counts.index(max(my_counts))])
 
+
+# Another way to solve it
+count = 0
+number = 0
+
+for n in num_list:
+    if num_list.count(n) > count:
+        count = num_list.count(n)
+        number = n
+
+print(n)
+
 # CHALLENGE PROBLEMS (2pts)
 # TOUGH PROBLEMS, BUT FEW POINTS
 
 # Find the number of prime numbers in num_list?
 # Hint: One way is to just start removing the ones that aren't
 
+def is_prime(n):
+    for i in range(2, n/2):
+        if n % 1 == 0:
+            return False
+    return True
+
 # Find the number of palindromes
 # Hint: This may be easier to do with strings
+
+
+string_list = [str(x) for x in num_list]
+
+def is_palindrome(my_string):
+    for i in range(len(my_string)):
+        if my_string[i] != my_string[-i -1]:
+            return False
+    return True
+
+print(is_palindrome("bob"))
+
+
